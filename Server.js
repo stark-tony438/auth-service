e2c84766552e0d06dd4809a22392e12799987c17
stark-tony438 <stark-tony438@users.noreply.github.com>
2024-09-16 11:25:16 +0000

    deploy: 0fd8a41645a0b92d9f38225e9bb489627ebff2ec

--- Server.js
+++ Server.js
@@ -6,8 +6,8 @@ const routes_1 = require("./routes");
 const DataBase_1 = require("./libs/DataBase");
 const cors = require("cors");
 //cluster method
-const cluster = require("cluster");
-const os = require("os");
+// import * as cluster from 'cluster';
+// import * as os from 'os';
 class Server {
     constructor(config) {
         this.config = config;
@@ -22,28 +22,27 @@ class Server {
             app.use(bodyParser.json());
         };
         this.run = () => {
-            if (cluster.isMaster) {
-                console.log(`Master ${process.pid} is running`);
-                const numCPUs = os.cpus().length;
-                for (let i = 0; i < numCPUs; i++) {
-                    cluster.fork();
-                }
-                cluster.on('exit', (worker, code, signal) => {
-                    console.log(`worker ${worker.process.pid} died`);
+            // if (cluster.isMaster) {
+            //   console.log(`Master ${process.pid} is running`);
+            //   const numCPUs = os.cpus().length;
+            //   for (let i = 0; i < numCPUs; i++) {
+            //     cluster.fork();
+            //   }
+            //   cluster.on('exit', (worker, code, signal) => {
+            //     console.log(`worker ${worker.process.pid} died`);
+            //   });
+            // } else {
+            const { app, config: { PORT: port, MONGO_URL: mongoUrl }, } = this;
+            DataBase_1.default.open(mongoUrl).then((res) => {
+                app.listen(port, () => {
+                    console.log(`:::App is running successfully at port number: ${port}:::::::`);
+                    console.log(`worker ${process.pid} on`);
                 });
-            }
-            else {
-                const { app, config: { PORT: port, MONGO_URL: mongoUrl }, } = this;
-                DataBase_1.default.open(mongoUrl).then((res) => {
-                    app.listen(port, () => {
-                        console.log(`:::App is running successfully at port number: ${port}:::::::`);
-                        console.log(`worker ${process.pid} on`);
-                    });
-                }).catch((err) => {
-                    console.log('ERROR');
-                    console.log(err);
-                });
-            }
+            }).catch((err) => {
+                console.log('ERROR');
+                console.log(err);
+            });
+            // }
         };
         this.setUpRoutes = () => {
             const { app } = this;
